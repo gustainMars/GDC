2c7452c7f57e319dc2261778656545ef48192efe
Gustavo Marques <gmartes23@gmail.com>
2021-10-13 23:00:11 -0300

    Created TaskListViewModel.kt, which is a bridge from Dao to View. Defined variables that receives TaskRepository and a LiveData List of Tasks, and initialized them on init, for keep things up on state changes, like turning the phone ex. addTask using viewModelScope from coroutines bc used suspended function, and insert data in another thread. TaskViewModelFactory standard implementation of create method.

---
+++ app/src/main/java/com/gustainmars/gdcapplication/feature/presentation/TaskListViewModel.kt
@@ -0,0 +1,42 @@
+package com.gustainmars.gdcapplication.feature.presentation
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.gustainmars.gdcapplication.base.AppDataBase
+import com.gustainmars.gdcapplication.feature.data.entity.TaskDto
+import com.gustainmars.gdcapplication.feature.data.local.TaskDao
+import com.gustainmars.gdcapplication.feature.data.repository.TaskRepository
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class TaskListViewModel (application: Application): AndroidViewModel(application) {
+
+    private val repository: TaskRepository
+    private val alltasks: LiveData<List<TaskDto>>
+
+    init {
+        val dao: TaskDao = AppDataBase.getDataBase(application).taskDao()
+        repository = TaskRepository.create(dao)
+        alltasks = repository.getAllTasks()
+
+    }
+
+    fun addTask(taskDto: TaskDto) {
+        viewModelScope.launch {
+            repository.addTask(taskDto)
+        }
+    }
+
+    class TaskViewModelFactory constructor(private val application: Application) :
+        ViewModelProvider.Factory {
+
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return if (modelClass.isAssignableFrom(TaskListViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                TaskListViewModel(this.application) as T
+            } else {
+                throw IllegalArgumentException("ViewModel not found")
+            }
+        }
+    }
+}
