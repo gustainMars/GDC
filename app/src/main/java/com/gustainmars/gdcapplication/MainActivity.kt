eaa70351845b682556e08d27ab919d750a7b05a9
Gustavo Marques <gmartes23@gmail.com>
2021-10-19 00:09:18 -0300

    First execution! Removed content_gdc.xml, fragment_first.xml, fragment_second.xml and nav_graph.xml bc unnecessary, updated activity_gdc.xml and AndroidManifest.xml to set the Main Activity, removed unused import on MainActivity.kt and some stuff generated from build

--- app/src/main/java/com/gustainmars/gdcapplication/MainActivity.kt
+++ app/src/main/java/com/gustainmars/gdcapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.gustainmars.gdcapplication
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
